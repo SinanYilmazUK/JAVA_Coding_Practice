9bd88666f22edad784989f275070376c2d558682
Sinan Yilmaz <cakimentoglu@gmail.com>
2022-06-23 00:04:50 +0100

    Launching first day of JAVA Review with coding

---
+++ src/main/java/TasksRelatedWith_STRING/String_anagram.java
@@ -0,0 +1,13 @@
+package TasksRelatedWith_STRING;
+
+public class String_anagram {
+
+    /*
+    Write a  function that check if a string is build out of the same letters as another string.
+            Ex: same("abc", "cab"); -> true , Solution 1:
+                same("abc", "abb"); -> false:
+
+     */
+
+
+}
